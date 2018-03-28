ddafa4003016bb166694306476f33e3ad371ea3e
jonesaustindev <jonesaustindev@gmail.com>
2018-03-27 21:20:44 -0500

    db

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require(process.env.MONGO_URI);
+  module.exports = require('./prod');
 } else {
   module.exports = require('./dev');
 }
