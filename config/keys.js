612c8abf681dad7849893da247bfe60df461e29e
jonesaustindev <jonesaustindev@gmail.com>
2018-03-27 20:05:39 -0500

    fixing db connection

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./prod');
+  module.exports = require('mongoURI: process.env.MONGO_URI');
 } else {
   module.exports = require('./dev');
 }
