eeab4898f11edb410bea2001f0c5ccbb44b994ee
jonesaustindev <jonesaustindev@gmail.com>
2018-03-27 19:56:29 -0500

    changing db setting

--- config/database.js
+++
@@ -1,9 +0,0 @@
-if(process.env.NODE_ENV === "production") {
-  module.exports = {
-    mongoURI: "mongodb://ajones:Shadow240@ds131826.mlab.com:31826/startify-prod"
-  }
-} else {
-  module.exports = {
-    mongoURI: "mongodb://localhost/startify-db"
-  }
-}
---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://localhost/startify-db"
+}
